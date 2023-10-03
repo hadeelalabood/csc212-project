927d066c50ea9e5ab0b55f15f7245cf7054f7d03
hadee <hadee@10.6.194.11>
2023-10-03 07:42:15 +0300

    class 1

--- csc212/src/csc212/Contact.java
+++ csc212/src/csc212/Contact.java
@@ -1,11 +1,74 @@
 package csc212;
 
 public class Contact implements Comparable<Contact> {
-
+	
+	private String name;
+	 private String phoneNumber;
+	 private String email;
+	 private String address;
+	 private String birthday;
+	 private String notes;
+	 
+	 
+	public Contact(String name, String phoneNumber, String email, String address, String birthday, String notes) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+		this.birthday = birthday;
+		this.notes = notes;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getBirthday() {
+		return birthday;
+	}
+	
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	//
 	@Override
 	public int compareTo(Contact o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.name.compareTo(o.name);
 	}
-
+//to string?
 }
