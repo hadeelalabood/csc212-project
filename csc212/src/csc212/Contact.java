650561a937e295dd5a3ecd64e564efff0bd99881
hadee <hadee@10.6.194.11>
2023-10-03 07:40:59 +0300

    class 1

---
+++ csc212/src/csc212/Contact.java
@@ -0,0 +1,11 @@
+package csc212;
+
+public class Contact implements Comparable<Contact> {
+
+	@Override
+	public int compareTo(Contact o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
